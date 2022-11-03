4fa3a1c2a7f91eebcca530d01f725893d44b41b0
Neros80 <a0415@hotmail.fr>
2022-11-03 15:28:00 +0100

    gestion des users

---
+++ accounts/views.py
@@ -0,0 +1,17 @@
+from django.contrib.auth import get_user_model,login
+from django.shortcuts import render, redirect
+
+User = get_user_model()
+
+def signup(request):
+    
+    if request.method == "POST":
+        #traiter le formulaire
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = User.objects.create_user(username=username,
+                                 password=password)
+        login(request, user)
+        return redirect('index') 
+                
+    return render(request, 'accounts/signup.html')
